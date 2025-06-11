d447f3583ba54db0b74292372461381f57573552
fee1-dead <ent3rm4n@gmail.com>
2025-06-11 06:43:14 +0000

    use orig message when deploying

--- build.ts
+++
@@ -1,7 +0,0 @@
-await Bun.build({
-  entrypoints: ['src/main.ts'],
-  outdir: './build',
-  minify: true,
-  banner: "// {{Wikipedia:USync |repo=https://github.com/fee1-dead/cplus |ref=refs/heads/production |path=main.js}}\n\
-// A helper for Special:CheckUser."
-});
