368ef4202d873bd3f9514528e303ca977932fb6c
dbeef <ent3rm4n@gmail.com>
2025-06-29 15:19:56 +0800

    improve load/mount reliability; better style; more classes for configurability

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,32 @@
 // https://stackoverflow.com/a/9899701/13854774
 function docReady(fn: () => any) {
     // see if DOM is already available
-    if (document.readyState === "complete" || document.readyState === "interactive") {
+    if (document.readyState === "complete") {
         // call on next available tick
         setTimeout(fn, 1);
     } else {
         document.addEventListener("DOMContentLoaded", fn);
     }
-}    
+}
 
 docReady(() => {
     if (mw.config.get("wgPageName") !== "Special:CheckUser") return;
+    // TODO switch to Codex and Vue see https://en.wikipedia.org/wiki/User:EGardner_(WMF)/codex-hello-world.js
+    const results = document.getElementById("checkuserresults");
+    if (!results) {
+        console.error("cplus: can't cu results!");
+        return;
+    }
+
+    mw.util.addCSS(`
+        .cplus-copybtn {
+            margin: 5px 0 5px 0;
+        }
+
+        .cplus-checkbox {
+            margin-right: 4px;
+        }
+    `)
 
     const users = document.querySelectorAll(".mw-checkuser-helper-table tbody tr td a.mw-userlink");
     const users2 = document.querySelectorAll(".mw-checkuser-user-link a.mw-userlink");
@@ -36,8 +52,7 @@ docReady(() => {
         const uwu = document.createElement("input");
         uwu.type = "checkbox"
         uwu.value = elem.firstElementChild?.innerHTML ?? "";
-        uwu.classList.add("cplus-injected")
-        uwu.classList.add(clazz);
+        uwu.classList.add("cplus-injected", "cplus-checkbox", clazz)
         elem.before(uwu);
     }
 
@@ -45,6 +60,11 @@ docReady(() => {
     users2.forEach(addCheckbox)
 
     const div = document.createElement("div");
+    const header = document.createElement("div");
+    header.innerText = "C+";
+    div.append(header);
+    header.classList.add("cplus-injected", "cplus-injected-header");
+
     const textArea = document.createElement("textarea");
 
     const parse = () => {
@@ -64,9 +84,9 @@ docReady(() => {
     };
     
     div.append(textArea);
-    div.classList.add("cplus-injected");
-    textArea.classList.add("cplus-injected");
-    textArea.style = "left: 100px; position: relative; float: left;";
+    div.classList.add("cplus-injected", "cplus-injected-top");
+    div.classList.add("oo-ui-panelLayout-padded", "oo-ui-panelLayout-framed");
+    textArea.classList.add("cplus-injected", "cplus-textarea");
     textArea.oninput = () => {
         const set = parse();
         map.forEach((clazz, username) => {
@@ -92,21 +112,16 @@ docReady(() => {
         })
     })
 
-    let btn = document.querySelector("div.mw-checkuser-helper-copy-text-layout div.oo-ui-fieldLayout-body div span.oo-ui-copyTextLayout-multiline-button")
-    if (!btn) {
-        console.error("cplus: can't find copy button for table!");
-        return;
-    }
+    const inject = results.previousElementSibling ?? results;
 
     const copybtn = document.createElement("button");
-    copybtn.style = "left: 105px; position: relative"
-    copybtn.classList.add("cplus-injected");
+    copybtn.classList.add("cplus-injected", "cplus-copybtn");
     copybtn.innerText = "Copy socklist";
     copybtn.addEventListener("click", () => {
         textArea.select();
         window.navigator.clipboard.writeText(textArea.value);
     });
     div.append(copybtn);
-    btn.after(div)
+    inject.before(div);
 })
 
